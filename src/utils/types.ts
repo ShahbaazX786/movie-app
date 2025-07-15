db559a9651c6ab87c67a8c685dc06c51097cc70d
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-07-15 13:18:23 +0530

    added types.

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,3 +6,24 @@ export type searchProps = {
 };
 
 export type changeEvent = React.ChangeEvent<HTMLInputElement>;
+
+export type MovieProps = {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type customMovieType = {
+  movie: MovieProps;
+};
