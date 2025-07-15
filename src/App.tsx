c6a636c3411bb7595d288e072764b5efe5009b07
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-07-15 11:47:47 +0530

    updated app component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 function App() {
   return (
-    <>
-      <h1 className="font-black text-black">Base</h1>
-    </>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll enjoy
+            without the hassle
+          </h1>
+          <p>Search</p>
+        </header>
+      </div>
+    </main>
   );
 }
 
