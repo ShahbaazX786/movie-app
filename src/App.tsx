4e8705e57b23373ddff2b89b76fdc7aa90db16dd
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-07-15 12:45:15 +0530

    Added api and tested in main component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./components/Search";
+import { API_OPTIONS, getUrl } from "./utils/helper";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMsg("");
+    try {
+      const url = getUrl();
+      const response = await fetch(url, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+      const data = await response.json();
+      if (data.Response === "false") {
+        setErrorMsg(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
+      console.log(data.results);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMsg("Error fetching movies, Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -16,6 +49,27 @@ function App() {
           </h1>
           <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
         </header>
+
+        <section className="all-movies">
+          <h2>All movies</h2>
+          {isLoading ? (
+            <p className="text-white">Loading...</p>
+          ) : errorMsg ? (
+            <p className="text-red-500">{errorMsg}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
+              ))}
+            </ul>
+          )}
+
+          {errorMsg && (
+            <p className="text-red-500 font-semibold text-lg">{errorMsg}</p>
+          )}
+        </section>
       </div>
     </main>
   );
